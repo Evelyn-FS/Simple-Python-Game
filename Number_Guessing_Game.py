72b88ea2c4e7d4620a034af7683487266223fc11
Evelyn-FS <evelyna.fatmasantoso@gmail.com>
2026-02-01 18:19:32 +0700

    second_commit

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -1,22 +1,20 @@
 import random
 
-print(" ")
-print("===================================")
-print(" ")
+# Home page and instructions
+print("\n===================================")
 print("Welcome to the Number Guessing Game")
-print(" ")
-print("===================================")
-print(" ")
+print("===================================\n")
+print("How to play:")
+print("1. You need to type a number between 1-100")
+print("2. You will be recive 3 chance to guess the correct number after your first guess is incorrect")
+print("3. Each chance (Attempts) you spend, will get you a different clue that will help you get closer to the correct number")
+print("\n===GOOD LUCK!===\n")
 
 # Generating a random number
 number = random.randint(1, 100)
 
-# Defining clues and attempts
-clue_number = [20, 10, 5]
-attempts = 0
-
 # Asking the user to give an input
-number_guessed = int(input("Please select a number from 1-100: "))
+first_guess = int(input("Please select a number from 1-100: "))
 
 # Function to mask the last digit
 def masked_last_digit(number):
@@ -27,6 +25,9 @@ def masked_last_digit(number):
 # Masked last digit of the number
 masked_number = masked_last_digit(number)
 
+# Defining clue ranges
+clue_number = [20, 15, 10, 5]
+
 # Calculating ranges for clues
 for i in range(len(clue_number)):
     less = masked_number - clue_number[i]
@@ -36,27 +37,40 @@ for i in range(len(clue_number)):
     if more > 100:
         more = 100
     if i == 0:
-        less1, more1 = less, more
+        free_clue = f"{less} and {more}"
     elif i == 1:
-        less2, more2 = less, more
+        clue1 = f"{less} and {more}"
     elif i == 2:
-        less3, more3 = less, more
+        clue2 = f"{less} and {more}"
+    elif i == 3:
+        clue3 = f"{less} and {more}"
+
+# Initial attempts
+attempts = 0
 
 # Function to provide clues based on attempts
-def clue(attempts):
-    if attempts == 1:
-        return f"The number is between {less1} and {more1}"
+def clue_output(attempts):
+    if attempts == 3:
+        return f"The number is between {clue1}\nYou have {attempts} attempts left"
     elif attempts == 2:
-        return f"The number is between {less2} and {more2}"
-    elif attempts == 3:
-        return f"The number is between {less3} and {more3}"
+        return f"The number is between {clue2}\nYou have {attempts} attempts left"
+    elif attempts == 1:
+        return f"The number is between {clue3}\nYou have {attempts} attempts left"
     return f"NICE TRY! The actual number is '{number}'"
 
 # Checking the user's input
-if number_guessed == number:
+if first_guess == number:
     print("Congrats your guess is correct")
-else:
-    while number_guessed != number and attempts <= 3:
-        attempts += 1
-        print(clue(attempts))
-        number_guessed = int(input("Your guess is incorrect, try again: "))
+elif first_guess != number:
+    attempts += 3
+    print(f"Your first guess is incorrect\nYou have received {attempts} attempts")
+    print(f"The number is between {free_clue}")
+    number_guessed = int(input("Please enter your next guess: "))
+    if number_guessed == number:
+        print("Congrats your guess is correct")
+    else:
+        while number_guessed != number and attempts != 0:
+            attempts -= 1
+            print(f"Attempts left: {attempts}")
+            print(clue_output(attempts))
+            number_guessed = int(input("Your guess is incorrect, try again: "))
