4b4d375f10ddc1710f17e5df7cadecdfdd0f6fe6
Evelyn-FS <evelyna.fatmasantoso@gmail.com>
2026-03-02 23:59:45 +0700

    modified:   Logic_NGG.py

--- Logic_NGG.py
+++ Logic_NGG.py
@@ -1,38 +1,106 @@
-    # Difficulty settings logic
+# Difficulty settings logic
+
+print("\n== Main Menu ==\n")
+
+mode = ""
+
+def current_difficulty(mode):
+    if mode == "Easy":
+        print("\n=> Your current difficulty level is >Easy<.\n")
+    elif mode == "Medium":
+        print("\n=> Your current difficulty level is >Medium<.\n")
+    elif mode == "Hard":
+        print("\n=> Your current difficulty level is >Hard<.\n")
+    else:
+        print("\n=> No difficulty level selected yet.\n")
+
 while True:
-    print("\n== Main Menu ==\n")
-    while True:
-        print("== Difficulty Settings Menu ==")
-        print("1. Easy")
-        print("2. Medium")
-        print("3. Hard")
-        print("4. Current level")
-        difficulty = input("> Type '0' to back to main menu: ")
+    print("== Difficulty Settings Menu ==")
+    print("1. Easy")
+    print("2. Medium")
+    print("3. Hard")
+    print("4. Current level")
+    settings = input("> Type '0' to back to main menu: ")
 
-        if difficulty == "1":
-            print("\n== Easy Mode Selected ==\n")
-            while difficulty == "1":
-                print("======Settings======")
-                print("1. Info")
-                print("0. Back to main menu")
-                print("====================")
-                easy = input("> Your choice: ")
-
-                if easy == "1":
-                    print("You will recieve 10 attempts to guess the number.\nThe clues will be more detailed and easier to understand.\nGood luck!")
-                    continue
-                elif easy == "0":
-                    print("\nSetting back to Difficulty Settings...\n")
-                    break
-                else:
-                    while easy not in ["1", "0"]:
-                        easy = input("Your choice is invalid, please try again: ")
-                        if easy in ["1", "0"]:
-                            break # Exit
+    if settings == "1":
+        mode = "Easy"
+        print("\n== Easy Mode Selected ==\n")
+        while settings == "1":
+            print("======Settings======")
+            print("1. Info")
+            print("0. Back to main menu")
+            print("====================")
+            difficulty = input("> Your choice: ")
+            if difficulty == "1":
+                print("You will recieve 10 attempts to guess the number.\nThe clues will be more detailed and easier to understand.\nGood luck!")
+                continue
+            elif difficulty == "0":
                 print("\nSetting back to Difficulty Settings...\n")
                 break
-        
-        elif difficulty == "2":
-            print("\n== Medium Mode Selected ==\n")
-            while difficulty == "2":
-                
+            else:
+                while difficulty not in ["1", "0"]:
+                    difficulty = input("Your choice is invalid, please try again: ")
+                    if difficulty in ["1", "0"]:
+                        break # Exit
+            print("\nSetting back to Difficulty Settings...\n")
+            break
+    
+    elif settings == "2":
+        print("\n== Medium Mode Selected ==\n")
+        mode = "Medium"
+        while settings == "2":
+            print("======Settings======")
+            print("1. Info")
+            print("0. Back to main menu")
+            print("====================")
+            difficulty = input("> Your choice: ")
+            if difficulty == "1":
+                print("You will recieve 7 attempts to guess the number.\nThe clues will be less detailed and more challenging to understand.\nGood luck!")
+                continue
+            elif difficulty == "0":
+                print("\nSetting back to Difficulty Settings...\n")
+                break
+            else:
+                while difficulty not in ["1", "0"]:
+                    difficulty = input("Your choice is invalid, please try again: ")
+                    if difficulty in ["1", "0"]:
+                        break # Exit
+            print("\nSetting back to Difficulty Settings...\n")
+            break
+
+    elif settings == "3":
+        print("\n== Hard Mode Selected ==\n")
+        mode = "Hard"
+        while settings == "3":
+            print("======Settings======")
+            print("1. Info")
+            print("0. Back to main menu")
+            print("====================")
+            difficulty = input("> Your choice: ")
+            if difficulty == "1":
+                print("You will recieve 5 attempts to guess the number.\nThe clues will be very vague and difficult to understand.\nGood luck!")
+                continue
+            elif difficulty == "0":
+                print("\nSetting back to Difficulty Settings...\n")
+                break
+            else:
+                while difficulty not in ["1", "0"]:
+                    difficulty = input("Your choice is invalid, please try again: ")
+                    if difficulty in ["1", "0"]:
+                        break # Exit
+            print("\nSetting back to Difficulty Settings...\n")
+            break
+
+    elif settings == "4":
+        current_difficulty(mode)
+        continue
+
+    elif settings == "0":
+        print("\nReturning to main menu...\n")
+        break
+
+    else:
+        while settings not in ["1", "2", "3", "4", "0"]:
+            settings = input("Your choice is invalid, please try again: ")
+            if settings in ["1", "2", "3", "4", "0"]:
+                break # Exit
